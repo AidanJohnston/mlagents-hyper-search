4bba86b608e909977b9b280d5b5673e897a99baf
Aidan Johnston <aidanjhnston@gmail.com>
2022-02-09 20:38:58 -0500

    Launches processes

--- mlagents-hyper-search.py
+++ mlagents-hyper-search.py
@@ -1,37 +1,45 @@
-import os
-import string
-from subprocess import Popen, CREATE_NEW_CONSOLE
 import argparse
-import yaml
+from concurrent.futures import ProcessPoolExecutor
+from logging import log
+import multiprocessing
+import os
+import pickle
+import time
+from threading import Thread
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+class MlagentsHyperSearch():
+
+    def __init__(self, config, name, nenv, configDir = "./config/", basePort = 5005):
+        self.config = config
+        self.name = name
+        self.nenv = nenv
+        self.configDir = configDir
+        self.basePort = basePort
+
+        for i in list(range(1, nenv + 1)):
+
+            p = Popen(f"mlagents-learn {self.config} --run-id={self.name}_{i} --base-port={self.basePort + i}",
+                      creationflags=CREATE_NEW_CONSOLE)
+            while p.poll() is None:
+                time.sleep(10)
+
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--configDir', '--cd', required=False, type=str,
-                        help="The folder where test config files will be stored.")
     parser.add_argument('--configFile', '--cf', required=True, type=str,
                         help="The configFile with search parameters defined.  If no configDir is being the folder of "
                              "this file be used as output for test config files.")
-
+    parser.add_argument('--nenv', '--ne', required=False, default=10)
+    parser.add_argument('--port', '--p', required=False, default=5005)
     args = vars(parser.parse_args())
 
     # Check if file exits
     if not os.path.isfile(args['configFile']):
         raise FileNotFoundError("Could not find " + args['configFile'])
 
-    # If Config folder not set use cf folder
-    if args['configDir'] is None:
-        args['configDir'] = os.path.dirname(args['configFile'])
+    mlagentsHyperSearch = MlagentsHyperSearch(args['configFile'], "testRun", 10, basePort=5005)
 
-    # Check if folder exits
-    elif not os.path.isdir(args['configDir']):
-        raise NotADirectoryError("Could not find " + args['configDir'])
-
-    # Loading yaml file
-    with open(args['configFile'], 'r') as file:
-        configFile = yaml.safe_load(file)
-
-
-    print(args)
 if __name__ == "__main__":
     main()
