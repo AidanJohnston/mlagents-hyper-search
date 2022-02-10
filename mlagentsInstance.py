804e394fa706949cdf72aa50f25ae87b785a7f99
Aidan Johnston <ajjohnst@lakeheadu.ca>
2022-02-10 11:16:20 -0500

    Init commit

---
+++ mlagentsInstance.py
@@ -0,0 +1,14 @@
+class mlagentsInstance():
+
+    def __init__(self, configFilePath, name, port, callback):
+
+        self.configFilePath = configFilePath
+        self.name = name
+        self.port
+
+    def kill():
+        p.kill()
+
+    def start():
+        self.p = Popen(f"mlagents-learn {self.configFilePath} --run-id={self.name} --base-port={self.port}",
+                           creationflags=CREATE_NEW_CONSOLE)
